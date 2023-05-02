59192b7ca6c4701ec05eb70638c7982de9d7c96f
Lucas Barbosa de Oliveira <lucasbarbosa2807@gmail.com>
2023-05-02 11:26:11 -0300

    test

---
+++ POCIAAlgorithm/POCIAAlgorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  POCIAAlgorithmApp.swift
+//  POCIAAlgorithm
+//
+//  Created by Lucas Barbosa de Oliveira on 02/05/23.
+//
+
+import SwiftUI
+
+@main
+struct POCIAAlgorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
